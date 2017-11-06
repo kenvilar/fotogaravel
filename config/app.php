9059fe22dd11d5ed0e9d89c1fa92329363ec5701
Ken Vilar <kenvilar@gmail.com>
2017-11-06 18:18:12 +0800

    Add the ide helper service provider to providers array

--- config/app.php
+++ config/app.php
@@ -176,6 +176,9 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+	    
+	    /* Laravel IDE Helper */
+	    Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
 
     ],
 
