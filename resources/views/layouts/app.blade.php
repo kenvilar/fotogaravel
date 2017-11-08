68cc38f4f55035b779b317b89f767fbe480fbd44
Ken Vilar <kenvilar@gmail.com>
2017-11-09 02:20:26 +0800

    Create main layout

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport"
+	      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<meta name="description"
+	      content="Fotogaravel is a simple, fast and light app to create a gallery of your photos using a Laravel framework.">
+	<title>Fotogaravel</title>
+</head>
+<body>
+
+</body>
+</html>
