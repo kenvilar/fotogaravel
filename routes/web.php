d5ec4fbf3112b014630ccec8fa4f0a99e6857d63
Ken Vilar <kenvilar@gmail.com>
2017-11-09 02:13:18 +0800

    Modify routes

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,6 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'AlbumsController@index');
+Route::get('/albums', 'AlbumsController@index');
+Route::get('/albums/create', 'AlbumsController@create');
