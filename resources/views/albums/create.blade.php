50bc71d363d35cde6aa73554ea9fa8ea22ac0234
Ken Vilar <kenvilar@gmail.com>
2017-11-09 03:54:34 +0800

    Open new html form

--- resources/views/albums/create.blade.php
+++ resources/views/albums/create.blade.php
@@ -2,4 +2,10 @@
 
 @section('content')
 	<h3>Create Album</h3>
+	{!! Form::open(['action' => 'AlbumsController@store', 'method' => 'POST', 'files' => true]) !!}
+	{{ Form::text('name', '', ['placeholder' => 'Album Name']) }}
+	{{ Form::textarea('description', '', ['placeholder' => 'Album Description']) }}
+	{{ Form::file('cover_image') }}
+	{{ Form::submit('Submit') }}
+	{!! Form::close() !!}
 @endsection
