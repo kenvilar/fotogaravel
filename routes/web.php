b89b7bba45096f0f9872b9835f0b8de3ddf15560
Ken Vilar <kenvilar@gmail.com>
2017-11-09 03:57:01 +0800

    Fix form by adding new route

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,4 @@
 Route::get('/', 'AlbumsController@index');
 Route::get('/albums', 'AlbumsController@index');
 Route::get('/albums/create', 'AlbumsController@create');
+Route::post('/albums/store', 'AlbumsController@store');
