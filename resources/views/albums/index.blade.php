8efa5cedd982c09a186bd47e8b52bcfd90d9e360
Ken Vilar <kenvilar@gmail.com>
2017-11-09 22:28:03 +0800

    Fix if-else conditional statements for displaying albums

--- resources/views/albums/index.blade.php
+++ resources/views/albums/index.blade.php
@@ -3,8 +3,43 @@
 @section('content')
 	<h3>Albums</h3>
 	@if(count($albums) > 0)
-		@foreach($albums as $album)
-			{{ $album->name }}
-		@endforeach
+        <?php
+        $albumCount = count($albums);
+        $i = 1;
+        ?>
+		<div id="albums">
+			<div class="row text-center">
+				@foreach($albums as $album)
+					@if($i == $albumCount)
+						<div class="medium-4 columns end">
+							<a href="/albums/{{ $album->id }}">
+								<img class="thumbnail" src="storage/album_covers/{{ $album->cover_image }}"
+								     alt="{{ $album->name }}">
+							</a>
+							<br>
+							<h4>{{ $album->name }}</h4>
+					@else
+						<div class="medium-4 columns">
+							<a href="/albums/{{ $album->id }}">
+								<img class="thumbnail" src="storage/album_covers/{{ $album->cover_image }}"
+								     alt="{{ $album->name }}">
+							</a>
+							<br>
+							<h4>{{ $album->name }}</h4>
+					@endif
+a
+					@if($i % 3 == 0)
+						</div></div>
+						<div class="row text-center">
+					@else
+						</div>
+					@endif
+				
+					<?php $i++; ?>
+				@endforeach
+			</div>
+		</div>
+	@else
+		<p>No albums found.</p>
 	@endif
 @endsection
