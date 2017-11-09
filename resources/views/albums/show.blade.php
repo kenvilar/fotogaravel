fa2cecf5c2e0683b0ef8de555707039a1cbd2af6
Ken Vilar <kenvilar@gmail.com>
2017-11-10 00:07:30 +0800

    Display all the photos inside album id

--- resources/views/albums/show.blade.php
+++ resources/views/albums/show.blade.php
@@ -5,4 +5,43 @@
 	<a href="{{ route('albums.index') }}" class="button secondary">Go Back</a>
 	<a href="/photos/create/{{ $album->id }}" class="button">Upload Photo to Album</a>
 	<hr>
+	@if(count($album->photos) > 0)
+        <?php
+        $photoCount = count($album->photos);
+        $i = 1;
+        ?>
+		<div id="photos">
+			<div class="row text-center">
+				@foreach($album->photos as $photo)
+					@if($i == $photoCount)
+						<div class="medium-4 columns end">
+							<a href="/storage/photos/{{ $photo->album_id }}/{{ $photo->photo }}">
+								<img class="thumbnail" src="/storage/photos/{{ $photo->album_id }}/{{ $photo->photo }}"
+								     alt="{{ $photo->title }}">
+							</a>
+							<br>
+							<h4>{{ $photo->title }}</h4>
+					@else
+						<div class="medium-4 columns">
+							<a href="/storage/photos/{{ $photo->album_id }}/{{ $photo->photo }}">
+								<img class="thumbnail" src="/storage/photos/{{ $photo->album_id }}/{{ $photo->photo }}"
+								     alt="{{ $photo->title }}">
+							</a>
+							<br>
+							<h4>{{ $photo->title }}</h4>
+					@endif
+
+					@if($i % 3 == 0)
+						</div></div><div class="row text-center">
+					@else
+						</div>
+					@endif
+
+                    <?php $i++; ?>
+				@endforeach
+			</div>
+		</div>
+	@else
+		<p>No photos found.</p>
+	@endif
 @endsection
