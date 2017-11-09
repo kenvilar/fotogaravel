af65af9194c90ad760c094435c8db05eaec6a514
Ken Vilar <kenvilar@gmail.com>
2017-11-09 22:44:59 +0800

    Fix album routes and make it short

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,4 @@
 */
 
 Route::get('/', 'AlbumsController@index');
-Route::get('/albums', 'AlbumsController@index');
-Route::get('/albums/create', 'AlbumsController@create');
-Route::post('/albums/store', 'AlbumsController@store');
+Route::resource('albums', 'AlbumsController');
